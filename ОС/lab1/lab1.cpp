cdd1bc8b899ad9046773ab40e982e67803f18607
Алексей Федоров <Fedorov.Alexey@live.ru>
2021-03-08 11:51:33 +0300

    Пробовал доработать первую лабу по ОС

--- ОС/lab1/lab1.cpp
+++ ОС/lab1/lab1.cpp
@@ -11,14 +11,15 @@ void* proc1(void* flag)
 {
     int *flag1=(int*)flag;
     while ((*flag1) ==0)
-
     {
 
-        cout << "1\n" ;
-        sleep(3);
+        // cout << "1" ;
+        printf("1");
+        sleep(1);
     }
-    
-    return nullptr;
+    //pthread_exit((void*)new int(3));
+    //return (void*)new int(3);
+    return 0;
 }
 
 
@@ -28,12 +29,14 @@ void* proc2(void* flag)
 {
     int *flag2=(int*)flag;
     while ((*flag2) ==0)
-
     {
-        cout << "2\n";
-        sleep(3);
+        // cout << "2";
+        printf("2");
+        sleep(1);
     }
-    return nullptr;
+    //pthread_exit((void*)new int(4));
+    //return (void*)new int(4);
+    return 0;
 }
 
 
@@ -43,23 +46,25 @@ int main()
 {
     int flag1= 0;
     int flag2= 0;
+    int* test1;
+    int* test2;
 
     pthread_t id1;
     pthread_t id2;
 
     pthread_create(&id1, NULL, proc1, &flag1);
-
     pthread_create(&id2, NULL, proc2, &flag2);
 
     getchar();
-
+    //sleep(10);
     flag1 = 1;
 
     flag2 = 1;
 
-    pthread_join(id1, NULL);
+    pthread_join(id1, NULL/* (void**)&test1*/);
+    pthread_join(id2, NULL/*(void**)&test2*/);
 
-    pthread_join(id2, NULL);
+    //cout<<endl<<(*test1)<<endl<<(*test2)<<endl;
     return 0;
 
 }
